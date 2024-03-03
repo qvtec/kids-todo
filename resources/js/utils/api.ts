b86a88ed8e29877c36e51708f6ded902d3a1731b
qvtec <qvtec3@gmail.com>
2024-03-03 23:11:22 +0900

    ⚡️ api toast remove

--- resources/js/utils/api.ts
+++ resources/js/utils/api.ts
@@ -1,57 +1,34 @@
-import { toast } from 'react-toastify'
-
 interface RequestOptions {
   method: string
   headers?: Record<string, string>
   body?: any
 }
 
-interface ToastOptions {
-  isShow?: boolean
-  successMessage?: string
-  errorMessage?: string
-}
-
-export async function get<T>(url: string, toastOptions: ToastOptions = { isShow: false }): Promise<T | undefined> {
-  return await api<T>(url, { method: 'GET' }, toastOptions)
+export async function get<T>(url: string): Promise<T | undefined> {
+  return await api<T>(url, { method: 'GET' })
 }
 
 export async function post<T>(
   url: string,
   body: any,
-  toastOptions: ToastOptions = {
-    isShow: true,
-    successMessage: '完了しました',
-    errorMessage: 'エラーが発生しました',
-  },
 ): Promise<T | undefined> {
-  return await api<T>(url, { method: 'POST', body }, toastOptions)
+  return await api<T>(url, { method: 'POST', body })
 }
 
 export async function put<T>(
   url: string,
   body: any,
-  toastOptions: ToastOptions = {
-    isShow: true,
-    successMessage: '完了しました',
-    errorMessage: 'エラーが発生しました',
-  },
 ): Promise<T | undefined> {
-  return await api<T>(url, { method: 'PUT', body }, toastOptions)
+  return await api<T>(url, { method: 'PUT', body })
 }
 
 export async function remove<T>(
   url: string,
-  toastOptions: ToastOptions = {
-    isShow: true,
-    successMessage: '削除しました',
-    errorMessage: 'エラーが発生しました',
-  },
 ): Promise<T | undefined> {
-  return await api<T>(url, { method: 'DELETE' }, toastOptions)
+  return await api<T>(url, { method: 'DELETE' })
 }
 
-export async function api<T>(url: string, options: RequestOptions = { method: 'GET' }, toastOptions?: ToastOptions): Promise<T | undefined> {
+export async function api<T>(url: string, options: RequestOptions = { method: 'GET' }): Promise<T | undefined> {
   try {
     const response = await fetch(url, {
       method: options.method,
@@ -63,15 +40,8 @@ export async function api<T>(url: string, options: RequestOptions = { method: 'G
     })
 
     const responseData: T = await response.json()
-    if (toastOptions?.isShow) {
-      toast.success(toastOptions.successMessage)
-    }
-
     return responseData
   } catch (error) {
-    if (toastOptions?.isShow) {
-      toast.error(toastOptions.errorMessage)
-    }
     console.error(error)
   }
 }
